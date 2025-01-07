3e7d5695a7a746eb968e8dd13b1dfbd7b4a0aac2
fange <fangetclement@gmail.com>
2025-01-07 20:25:54 +0100

    première version

---
+++ cmake-build-release-mingw/CMakeFiles/CalculMoyenneNotes.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CalculMoyenneNotes.dir/main.cpp.obj"
+  "CMakeFiles/CalculMoyenneNotes.dir/main.cpp.obj.d"
+  "CalculMoyenneNotes.exe"
+  "CalculMoyenneNotes.exe.manifest"
+  "CalculMoyenneNotes.pdb"
+  "libCalculMoyenneNotes.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CalculMoyenneNotes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
